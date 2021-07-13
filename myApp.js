db7b12e93298f396a011ad17b22d7381551e535e
Yufan Lou <2263580+louy2@users.noreply.github.com>
2021-07-13 15:02:59 -0400

    add(secu): no cache

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({ maxAge: 90*24*60*60, force: true }));
 app.use(helmet.dnsPrefetchControl());
-
+app.use(helmet.noCache());
 
 
 
