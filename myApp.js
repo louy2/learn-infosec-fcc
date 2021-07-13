4dd4ac3d8d01af0c92ae8801db811471f5771cb2
Yufan Lou <2263580+louy2@users.noreply.github.com>
2021-07-13 15:06:49 -0400

    fix(secu): Content Security Policy

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
 app.use(helmet.contentSecurityPolicy({
   defaultSrc: ["'self'"],
-  scriptSrc: ["'self'", "'trusted-cdn.com'"],
+  scriptSrc: ["'self'", "trusted-cdn.com"],
 }));
 
 
